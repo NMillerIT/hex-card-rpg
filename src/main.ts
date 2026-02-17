93aaeace7f5b70f14a9004bcdb48cd19f1ba61db
NMillerIT <nmmiller1991@gmail.com>
2026-02-17 15:22:14 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,6 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const app = document.getElementById("app")!
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+app.innerHTML = `
+  <h1>Hex Card RPG</h1>
+  <button id="turn">End Turn</button>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
